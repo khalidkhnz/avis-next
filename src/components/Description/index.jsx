a66d4afaa5b6f0707919f92a39b72d9cbc703334
Khalid <khalid@sportjacks.com>
2024-07-23 13:03:49 +0530

    [init]

---
+++ src/components/Description/index.jsx
@@ -0,0 +1,45 @@
+import styles from "./style.module.scss";
+import { useInView, motion } from "framer-motion";
+import { useRef } from "react";
+import { slideUp, opacity } from "./animation";
+import Rounded from "../../common/RoundedButton";
+export default function Index() {
+  const phrase =
+    "APCO’s public affairs heritage and shared passion for issues is at the core of our strategic mindset.";
+  const description = useRef(null);
+  const isInView = useInView(description);
+  return (
+    <div ref={description} className={styles.description}>
+      <div className={styles.body}>
+        <p>
+          {phrase.split(" ").map((word, index) => {
+            return (
+              <span key={index} className={styles.mask}>
+                <motion.span
+                  variants={slideUp}
+                  custom={index}
+                  animate={isInView ? "open" : "closed"}
+                  key={index}
+                >
+                  {word}
+                </motion.span>
+              </span>
+            );
+          })}
+        </p>
+        <motion.p variants={opacity} animate={isInView ? "open" : "closed"}>
+          We work with our clients to help them tell their stories through
+          authentic voices of allies and partners to create action and authentic
+          change in the world. Our deep bench of experts and partners from major
+          business, financial, political and media capitals understand advocacy
+          and gets results.
+        </motion.p>
+        <div data-scroll data-scroll-speed={0.1}>
+          <Rounded className={styles.button}>
+            <p>About us</p>
+          </Rounded>
+        </div>
+      </div>
+    </div>
+  );
+}
---
+++ src/components/Landing/index.jsx
@@ -0,0 +1,60 @@
+"use client";
+import { useRef, useLayoutEffect } from "react";
+import gsap from "gsap";
+import { ScrollTrigger } from "gsap/all";
+import { slideUp } from "./animation";
+import { motion } from "framer-motion";
+
+export default function Home() {
+  const firstText = useRef(null);
+  const secondText = useRef(null);
+  const slider = useRef(null);
+  let xPercent = 0;
+  let direction = -1;
+
+  useLayoutEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.to(slider.current, {
+      scrollTrigger: {
+        trigger: document.documentElement,
+        scrub: 0.25,
+        start: 0,
+        end: window.innerHeight,
+        onUpdate: (e) => (direction = e.direction * -1),
+      },
+      x: "-500px",
+    });
+    requestAnimationFrame(animate);
+  }, []);
+
+  const animate = () => {
+    if (xPercent < -100) {
+      xPercent = 0;
+    } else if (xPercent > 0) {
+      xPercent = -100;
+    }
+    gsap.set(firstText.current, { xPercent: xPercent });
+    gsap.set(secondText.current, { xPercent: xPercent });
+    requestAnimationFrame(animate);
+    xPercent += 0.1 * direction;
+  };
+
+  return (
+    <motion.main
+      variants={slideUp}
+      initial="initial"
+      animate="enter"
+      className="relative w-full"
+    >
+      <div className="absolute w-full h-full flex items-end ">
+        <h1 className="text-white m-6">Avis Media 2024</h1>
+      </div>
+      <div className="w-full h-full">
+        <video autoPlay loop muted className="w-full h-full">
+          <source src="/landing.mp4" type="video/mp4" />
+          Your browser does not support the video tag.
+        </video>
+      </div>
+    </motion.main>
+  );
+}
